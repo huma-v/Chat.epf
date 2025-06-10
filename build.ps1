bc38d52afc2d5b9b140c38ef43e4e9d232e6e337
huma-v <13147304+huma-v@users.noreply.github.com>
2025-06-10 06:49:14 +0300

    Перша версія

---
+++ build.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = "Stop"
+
+# Read .env file into environment variables
+if (Test-Path .env -PathType Leaf) {
+    Get-Content .env | ForEach-Object {
+        $name, $value = $_.split('=')
+        Set-Item env:$name -Value $value
+    }
+}
+
+# Create build output directory
+New-Item .\build -ItemType Directory -Force
+
+# Build the epf
+$designer = Start-Process "${env:1C_HOME}\bin\1cv8.exe" "DESIGNER /LoadExternalDataProcessorOrReportFromFiles .\ChatEpf\ChatEpf.xml .\build\Chat.epf" -PassThru -Wait
+if ($designer.ExitCode -ne 0) {
+	throw "Error building epf!";
+}
